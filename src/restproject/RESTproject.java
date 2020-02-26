1835d6ab19ec1e66d5dd3493993b2b1c66cd958f
lai2 <informatica@lai2>
2020-02-26 10:57:51 +0100

    Aggiornamento 26/02/2020

--- src/restproject/RESTproject.java
+++ src/restproject/RESTproject.java
@@ -52,7 +52,7 @@ public class RESTproject {
 	  }
 
 	}
-    public void post(){
+    public void post(String dates[]){
         	  try {
 
 		URL url = new URL("http://localhost:8080/api/tutorial/1.0/employees");
@@ -61,12 +61,7 @@ public class RESTproject {
 		conn.setRequestMethod("POST");
 		conn.setRequestProperty("Content-Type", "application/json");
 
-		String input = "{ \\ \n" +
-"   \"employeeId\": 1, \\ \n" +
-"   \"firstName\": \"Giorgio\", \\ \n" +
-"   \"lastName\": \"Stramaroni\", \\ \n" +
-"   \"email\": \"giorgiostramaroni%40gmail.com\", \\ \n" +
-"   \"phone\": \"34758569856\" \\ }";
+		String input = "{\"employeeId\":"+dates[0]+", \"firstName\": \""+dates[1]+", \"lastName\": \""+dates[2]+", \"email\": \""+dates[3]+", \"phone\": \""+dates[4]+"\"}";
 
 		OutputStream os = conn.getOutputStream();
 		os.write(input.getBytes());
@@ -76,9 +71,62 @@ public class RESTproject {
 			throw new RuntimeException("Failed : HTTP error code : "
 				+ conn.getResponseCode());
 		}
+                System.out.println("Registration successful");
+                get();
+                conn.disconnect();
+
+	  } catch (MalformedURLException e) {
+
+		e.printStackTrace();
+
+	  } catch (IOException e) {
+
+		e.printStackTrace();
+
+	 }
+    }
+    public void delete(String id){
+                try {
+		URL url = new URL("http://localhost:8080/api/tutorial/1.0/employees/"+id);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod("DELETE");
+		conn.setRequestProperty("Content-Type", "application/json");
+
+                if (conn.getResponseCode() != 200) {  
+			throw new RuntimeException("Failed : HTTP error code : "
+				+ conn.getResponseCode());
+                }
+                System.out.println("Delete successful");
+                get();
+                conn.disconnect();
+
+	  } catch (MalformedURLException e) {
+
+		e.printStackTrace();
+
+	  } catch (IOException e) {
+
+		e.printStackTrace();
+
+	 }
+    }
+    public void getId(String id){
+        	  try {
+		URL url = new URL("http://localhost:8080/api/tutorial/1.0/employees/"+id);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setDoOutput(true);
+		conn.setRequestMethod("GET");
+                conn.setRequestProperty("Content-Type", "application/json");
+		conn.setRequestProperty("Accept", "application/json");
+                
+		if (conn.getResponseCode() != 200) { 
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ conn.getResponseCode());
+		}
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(
-				(conn.getInputStream())));
+			(conn.getInputStream())));
 
 		String output;
 		System.out.println("Output from Server .... \n");
@@ -87,6 +135,40 @@ public class RESTproject {
 		}
 
 		conn.disconnect();
+
+	  } catch (MalformedURLException e) {
+
+		e.printStackTrace();
+
+	  } catch (IOException e) {
+
+		e.printStackTrace();
+
+	  }
+
+    }
+    public void postId(){
+                try {
+                String id ="55";
+		URL url = new URL("http://localhost:8080/api/tutorial/1.0/employees/"+id);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod("PUT");
+		conn.setRequestProperty("Content-Type", "application/json");
+
+		String input = "{\"employeeId\": 69, \"firstName\": \"Giacomo\", \"lastName\": \"Tafazzi\", \"email\": \"giacomotafazzi@gmail.com\", \"phone\": \"3556479514\"}";
+
+		OutputStream os = conn.getOutputStream();
+		os.write(input.getBytes());
+		os.flush();
+
+		                if (conn.getResponseCode() != 200) { //ERRORE DEL POST (CODICE 201) 
+			throw new RuntimeException("Failed : HTTP error code : "
+				+ conn.getResponseCode());
+		}
+                System.out.println("Registration successful");
+                get();
+                conn.disconnect();
 
 	  } catch (MalformedURLException e) {
 
--- src/restproject/Run.java
+++ src/restproject/Run.java
@@ -5,6 +5,8 @@
  */
 package restproject;
 
+import java.util.Scanner;
+
 /**
  *
  * @author informatica
@@ -12,9 +14,44 @@ package restproject;
 public class Run {
     public static void main(String[] args) {
         RESTproject r=new RESTproject();
-        r.get();
-        r.post();
+        int select;
+        do
+        {
+        System.out.println("Select an option");
+        Scanner myObj1 = new Scanner(System.in);
+        select = myObj1.nextInt();
+        switch(select)
+        {
+            case 0:
+                    return;
+            case 1:
+                r.get();
+                    break;
+            case 2:
+                String[] dates = new String[5];
+                for(int i = 0; i<5; i++)
+                {
+                    Scanner myObjP = new Scanner(System.in);
+                    dates[i]= myObjP.nextLine(); 
+                }
+                r.post(dates);
+                    break;
+            case 3:
+                System.out.println("Insert id for delete");
+                Scanner myObjD = new Scanner(System.in);
+                String idD = myObjD.nextLine();
+                r.delete(idD);
+                    break;
+            case 4:
+                System.out.println("Insert id for get");
+                Scanner myObjG = new Scanner(System.in);
+                String idG = myObjG.nextLine();
+                r.getId(idG);
+                    break;
+            case 5:
+                r.postId();
+                    break;
+        }
+        }while(select != 0);
     }
-    
-    
 }
